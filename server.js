a814fb41a40f748edc34f3dc42eae295edd4d383
Felix-Suen <felix.suen2000@gmail.com>
2021-01-04 18:23:39 -0800

    production

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 // connected database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use(cors());
 app.use('/api/game', require('./routes/api/game'));
+
+// Serve production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
